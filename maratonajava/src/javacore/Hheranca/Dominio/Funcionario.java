52e77d36a6c78438a4d9ce046f964014689513a5
Matheus Carneiro <matheusalc017@gmail.com>
2024-08-06 02:07:58 -0300

    Herança pt 01

---
+++ maratonajava/src/javacore/Hheranca/Dominio/Funcionario.java
@@ -0,0 +1,13 @@
+package javacore.Hheranca.Dominio;
+
+public class Funcionario extends Pessoa {
+    private Double salario;
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+}
---
+++ maratonajava/src/javacore/Hheranca/Test/HerancaTest01.java
@@ -0,0 +1,18 @@
+package javacore.Hheranca.Test;
+
+import javacore.Hheranca.Dominio.Endereco;
+import javacore.Hheranca.Dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("RUA: Manoel marinho");
+        endereco.setCep("CEP= 1223920.20");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Matheus");
+        pessoa.setCpf("CPF= 155243638120");
+        pessoa.setEndereco(endereco);
+        
+        pessoa.imprime();
+    }
+}
