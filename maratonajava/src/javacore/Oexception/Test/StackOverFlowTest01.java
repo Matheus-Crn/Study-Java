3ac4c34aa33b682356b50214a354afbf55296c17
Matheus Carneiro <matheusalc017@gmail.com>
2024-08-22 21:46:39 -0300

    Exception-pt01

---
+++ maratonajava/src/javacore/Oexception/Test/StackOverFlowTest01.java
@@ -0,0 +1,12 @@
+package javacore.Oexception.Test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+
+        recursividade(); // dessa forma vai acontecer o ERRO de stackOverFlow e estourar o limite da memoria 
+
+    }
+    public static void recursividade(){
+        recursividade();
+    }
+}
