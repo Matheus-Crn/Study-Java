e8ea67bcf64af667357b320e2193f48238cb889d
Matheus Carneiro <matheusalc017@gmail.com>
2024-08-29 00:22:10 -0300

    Update WrappersTest01.java

--- maratonajava/src/javacore/Pwrappers/Test/WrappersTest01.java
+++ maratonajava/src/javacore/Pwrappers/Test/WrappersTest01.java
@@ -3,6 +3,7 @@ package javacore.Pwrappers.Test;
 public class WrappersTest01 {
     public static void main(String[] args) {
         // dessa forma ele é byte code
+        // Tipos primitivos
         byte byteP = 1;
         short shortP = 1;
         int intP = 1;
@@ -11,8 +12,8 @@ public class WrappersTest01 {
         double doubleP = 10D;
         char charP = 'A';
         boolean booleanP = false;
-
-        //dessa forma ele usa o polimorfismo
+        //dessa forma ele usa o polimorfismo e são object - Wrappers
+        // autoboxing
         byte byteW = 1;
         short shortW = 1;
         int intW = 1;
@@ -21,5 +22,22 @@ public class WrappersTest01 {
         double doubleW = 10D;
         char charW = 'A';
         boolean booleanW = false;
+        // unboxing
+        int i = intW; // dessa forma eu converto de wrappers para tipo primitivo
+        Integer intW2 = Integer.parseInt("11");
+        boolean verdadeiro = Boolean.getBoolean(null);
+        System.out.println(verdadeiro);
+
+        double d = 135.4;
+        int intergerValue = (int) d;
+        System.out.println(intergerValue); //Embora o valor duplo seja explicitamente convertido em valor inteiro, também chamado de downcasting.
+
+        System.out.println(Character.isDigit('a'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit(1));
+        System.out.println(Character.isUpperCase('a'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.isUpperCase('a'));
+        System.out.println(Character.isLowerCase('a'));
     }
 }
